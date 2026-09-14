71b691762adda98b6c4999bd42f61282927f565e
laureanomeyer <laureanomeyer22@gmail.com>
2026-09-14 12:39:20 -0300

    changed runner cam

--- Assets/Scripts/UIs/PlayerCooldownUI.cs
+++ Assets/Scripts/UIs/PlayerCooldownUI.cs
@@ -13,10 +13,12 @@ public class PlayerCooldownUI : MonoBehaviour, IPunObservable
     {
         if (ownerView == null) ownerView = GetComponentInParent<PhotonView>();
 
-        if (!PhotonNetwork.IsMasterClient)
+        if (!ownerView.IsMine) return;
+
+        if (!PhotonManager.Instance.IsLocalPlayerTrapMaster())
         {
-            pushCooldownFill.transform.rotation = new Quaternion(-90f, transform.rotation.y, transform.rotation.z, transform.rotation.w);
-            grabCooldownFill.transform.rotation = new Quaternion(-90f, transform.rotation.y, transform.rotation.z, transform.rotation.w);
+            pushCooldownFill.transform.localRotation = Quaternion.Euler(-90f, 0f, 0f);
+            grabCooldownFill.transform.localRotation = Quaternion.Euler(-90f, 0f, 0f);
         }
     }
 
