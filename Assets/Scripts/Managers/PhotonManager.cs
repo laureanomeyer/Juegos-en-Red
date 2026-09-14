feeec02ac015eab32cde027bd8c52c23e6532a94
oliveriofogiel <oliifogiel@gmail.com>
2026-09-14 14:37:50 -0300

    Room exit on lobby polished

--- Assets/Scripts/Managers/PhotonManager.cs
+++ Assets/Scripts/Managers/PhotonManager.cs
@@ -260,7 +260,7 @@ public class PhotonManager : MonoBehaviourPunCallbacks
     public void LeaveRoomIntentionally()
     {
         intentionalDisconnect = true;
-        PhotonNetwork.LeaveRoom();
+        PhotonNetwork.LeaveRoom(becomeInactive: false);
     }
 
     public override void OnDisconnected(DisconnectCause cause)
